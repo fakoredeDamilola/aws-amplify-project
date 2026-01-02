ef676dd85c0446968d11440e6c8513d9cc043096
Fakorede <dammy.fakorede@gmail.com>
2026-01-02 08:25:23 +0100

    added delete functionality and authenicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const {signOut} = useAuthenticator();
 
 const client = generateClient<Schema>();
 
@@ -18,13 +20,17 @@ const client = generateClient<Schema>();
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id:string) {
+    client.models.Todo.delete({id});
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -34,6 +40,7 @@ const client = generateClient<Schema>();
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign Out</button>
     </main>
   );
 }
